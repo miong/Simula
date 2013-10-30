e03b7b715174b937ef8c379f4f8b0fbdf9c6a0dc
PANCARTE Kenny <kenny.pancarte@gmail.com>
2013-10-30 11:34:04 +0100

    Ajout skeleton Engine-Empty

--- skeleton/Engine/AbstractModel.scala
+++ skeleton/Engine/AbstractModel.scala
@@ -1,3 +1,15 @@
+/**
+ * *******************************************************
+ * SimulaGame
+ * Engine
+ * ll.scala
+ * (c)SimulaGame on 30 oct. 2013 11:30:30
+ * By ken
+ * Update 30 oct. 2013 11:30:30
+ * *******************************************************
+ */
+package Engine
+
 trait AbstractModel {
 
 }
