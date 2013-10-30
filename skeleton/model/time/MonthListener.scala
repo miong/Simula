aaada693be5345710681ba395bc458844e69a542
Sylvain <sylvain.carpe@infonie.fr>
2013-10-30 11:29:45 +0100

    structure model + testmodel + generation templates

--- skeleton/model/time/MonthListener.scala
+++ skeleton/model/time/MonthListener.scala
@@ -1,3 +1,17 @@
+/**
+ * *******************************************************
+ * Simula
+ *
+ * test2.scala
+ * (c)Simula on 30 oct. 2013 11:19:05
+ * By Scarpe
+ * Update 30 oct. 2013 11:19:05
+ * *******************************************************
+ */
+/**
+ * @author Scarpe
+ *
+ */
 package model.time
 
 trait MonthListener {
