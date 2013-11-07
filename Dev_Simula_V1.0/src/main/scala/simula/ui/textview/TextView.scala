75d893621d25cc625ed0d167c3644073fb30642a
Sylvain <sylvain.carpe@infonie.fr>
2013-11-07 13:22:03 +0100

    correction bug UI

--- Dev_Simula_V1.0/src/main/scala/simula/ui/textview/TextView.scala
+++ Dev_Simula_V1.0/src/main/scala/simula/ui/textview/TextView.scala
@@ -22,9 +22,11 @@ import main.scala.simula.ui.InterfaceType._
 class TextView(ri: RetrievedInformationInterface,
 				ae: AbstractEngine, 
 				size: Size, loc: Location) extends UIInterface {
-	maxSize = size
-	actualLocation = loc
-	engine = ae
+	var maxSize = size
+	var actualLocation = loc
+	var engine = ae
+	var actualState = ri
+	
 	def redefineView = println("CLIVIEW RedefineView Called")
 	def updateView (d: RetrievedInformationInterface) = println("CLIVIEW UpdateView Called")
 }
