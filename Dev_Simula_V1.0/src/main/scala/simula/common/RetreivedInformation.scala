264a8895f03d1b103c25ba2c17707238c327f360
Mion Giovanni <mion.ggb@gmail.com>
2013-11-07 16:48:00 +0100

    skeleton update - dev version main running

---
+++ Dev_Simula_V1.0/src/main/scala/simula/common/RetreivedInformation.scala
@@ -0,0 +1,34 @@
+/**
+ * *******************************************************
+ * Simula
+ * scanner
+ * RetreivedInformation.scala
+ * (c)Simula on 1 nov. 2013 13:21:16
+ * By Bubul
+ * Update 1 nov. 2013 13:21:16
+ * *******************************************************
+ */
+package simula.common
+
+/**
+ * @author Bubul
+ *
+ */
+class RetreivedInformation(val numberOfCitizen: Integer,
+  val globalPolution: Integer,
+  val globalCriminality: Integer)
+  extends RetreivedInformationInterface {
+
+  def getNumberOfCitizen(): Integer = {
+    return numberOfCitizen
+  }
+
+  def getGlobalPolution(): Integer = {
+    return globalPolution
+  }
+
+  def getGlobalCriminality: Integer = {
+    return globalCriminality
+  }
+
+}
