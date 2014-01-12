a100153a35be489af4fca8f908e4c65351acd465
Mion Giovanni <mion.ggb@gmail.com>
2014-01-12 17:53:48 +0100

    Ajout de la selection de l'interface utilisateur
    Selection de l'interface par lecture d'un entier lors de la création du
    jeu dans simulaLaucher.

--- Dev_Simula_V1.2/src/main/scala/simula/SimulaLauncher.scala
+++ Dev_Simula_V1.2/src/main/scala/simula/SimulaLauncher.scala
@@ -21,11 +21,13 @@ import main.scala.simula.ui._
 import main.scala.simula.adapters._
 import main.scala.simula.time._
 import main.scala.simula.ui.InterfaceType._
+import java.util.Scanner
 
 object SimulaLauncher {
 
   def main(args: Array[String]) {
 
+	val reader : Scanner = new Scanner(System.in)
     val viewBuilder: ViewBuilderInterface = new ViewBuilder()
     val engineBuilder: EngineBuilderInterface = new EngineBuilder()
     val modelBuilder: ModelBuilderInterface = new ModelBuilder()
@@ -34,7 +36,24 @@ object SimulaLauncher {
     val modelEngineAdapter: ModelEngineAdapterInterface = new ModelEngineAdapter()
 
     val mod: ModelInterface = modelBuilder.ModelBuilder()
-    val ui: UIInterface = viewBuilder.buildView(GUI, uiEngineAdapter, mod.giveInformations())
+    println("Quel type d'interface de jeu voulez vous ?")
+    println("1 - graphique (SWING)")
+    println("2 - Text (Console) ")
+    var interfaceType : InterfaceType = GUI
+    var rep = 0;
+    try {
+    	rep = reader.nextInt()
+    }catch {
+      case _ : Throwable =>
+    	println("Invalide value... \n Defaulte choice : Swing" )
+    	rep = 1
+    }
+    rep match {
+	  case 1 => interfaceType = GUI
+	  case 2 => interfaceType = TEXT
+	  case _ => println("Invalide value... \n Defaulte choice : Swing" )
+	}
+    val ui: UIInterface = viewBuilder.buildView(interfaceType, uiEngineAdapter, mod.giveInformations())
     val eng: EngineInterface = engineBuilder.build(modelEngineAdapter, uiEngineAdapter)
 
     uiEngineAdapter.INIT(ui, eng)
